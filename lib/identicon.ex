0406d3888616f2cc867d543caf28385f56221e0f
Stephen Koo <stephenyrkoo@gmail.com>
2017-10-13 23:29:36 +1100

    Return grid with just lise of colored values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -44,8 +44,10 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code, _index}) ->
+    grid = Enum.filter grid, fn({code, _index}) ->
       rem(code, 2) == 0
     end
+
+    %Identicon.Image{image | grid: grid}
   end
 end
